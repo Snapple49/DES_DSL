f02502d6a24fa6ea3fe036ff4b9c7c5f6443d2d4
Snapple49 <oliver-dot-stein@hotmail.com>
2016-12-13 17:42:47 +0100

    added test files

---
+++ lel/src/root/MasterBTTesting.java
@@ -0,0 +1,96 @@
+package root;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.NXTLightSensor;
+import lejos.remote.nxt.BTConnector;
+import lejos.remote.nxt.NXTConnection;
+
+public class MasterBTTesting {
+	private static String readVal;
+	private static DataInputStream reader;
+	private static PrintWriter writer;
+	
+	private static SensorManager sm;
+	private static SensorUpdater su;
+	private static EV3ColorSensor color;
+	private static NXTLightSensor llight;
+	private static NXTLightSensor rlight;
+	private static EV3UltrasonicSensor bultrasonic;
+	
+	private static int setUpCommMaster(PrintWriter writer, DataInputStream reader){
+		int success = 0;
+		String self = LocalEV3.get().getName();
+		String other;
+		switch (self) {
+		case "Rover5":
+			other = "Rover6";
+			break;
+		default:
+			other = "Rover8";
+			break;
+		}
+		BTConnector connector = new BTConnector();
+		NXTConnection connection = connector.connect(other, NXTConnection.RAW);
+		writer = new PrintWriter(connection.openOutputStream());
+		writer.println("REQUEST:CONNECT");
+		writer.flush();
+		System.out.println("Request sent");
+		
+		reader = new DataInputStream(connection.openDataInputStream());
+		
+		
+		try {
+			readVal = reader.readLine();
+			switch (readVal) {
+			case "ACK:CONNECT":
+				success = 1;
+				Sound.beepSequenceUp();
+				System.out.println("Connection success!" + readVal);
+				break;
+			default:
+				Sound.beepSequence();
+				break;
+			}
+			
+		} catch (IOException e) {
+			Sound.buzz();
+			e.printStackTrace();
+		}
+		
+		return success;
+	}
+	
+	public static void main(String[] args) {
+		int retval = setUpCommMaster(writer, reader);
+		while(retval != 1){
+			retval = setUpCommMaster(writer, reader);
+		}
+		llight = new NXTLightSensor(SensorPort.S1);
+		rlight = new NXTLightSensor(SensorPort.S2);
+		bultrasonic = new EV3UltrasonicSensor(SensorPort.S3);
+		color = new EV3ColorSensor(SensorPort.S4);
+		
+		sm = new SensorManager(color, llight, rlight, bultrasonic);
+		sm.start();
+		System.out.println("Sensormanager up");
+		
+		su = new SensorUpdater(sm, reader);
+		su.start();
+		System.out.println("Sensorupdater up");
+		
+		while(true){
+			
+		}
+	}
+}
---
+++ lel/src/root/SlaveBTTesting.java
@@ -0,0 +1,71 @@
+package root;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import lejos.hardware.Sound;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.remote.nxt.BTConnector;
+import lejos.remote.nxt.NXTConnection;
+
+public class SlaveBTTesting {
+	
+	private static String readValue;
+	private static DataInputStream reader;
+	private static PrintWriter writer;
+	
+	private static EV3UltrasonicSensor frontU;
+	private static EV3TouchSensor leftT;
+	private static EV3TouchSensor rightT;
+	private static EV3GyroSensor gyroS;
+	private static SlaveSensorManager ssm;
+	
+	
+	
+	private static int setUpCommSlave(PrintWriter writer, DataInputStream reader){
+		int success = 0;
+		BTConnector connector = new BTConnector();
+		NXTConnection connection = connector.waitForConnection(60000, NXTConnection.RAW);
+		System.out.println("Connection received!");
+		writer = new PrintWriter(connection.openOutputStream());
+		reader = new DataInputStream(connection.openDataInputStream());
+		System.out.println("Reader and writer up!");
+		
+		try {
+			readValue = reader.readLine();
+		} catch (IOException e) {
+			Sound.buzz();			
+		}
+
+		if(readValue.equals("REQUEST:CONNECT")){
+			Sound.beepSequenceUp();
+			writer.println("ACK:CONNECT");
+			writer.flush();
+			System.out.println("Communication setup!");
+			success = 1;
+		}
+		return success;
+	}
+	
+	public static void main(String[] args) {
+		int retval = setUpCommSlave(writer, reader);
+		while (retval != 1){
+			setUpCommSlave(writer, reader);
+		}
+		
+		ssm = new SlaveSensorManager(writer, frontU, leftT, rightT, gyroS, 1000);
+		ssm.start();
+		System.out.println("Ssm started!");
+		
+		while(true){
+			
+		}
+		
+	}
+	
+}
